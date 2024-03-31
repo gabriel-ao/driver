1cb6ad11b9788b462f3dc0b644eb9db356c55b04
DESKTOP-98QJDF1\gabri <gabrielao.developer@gmail.com>
2024-03-30 23:27:44 -0300

    Working procs to rent

---
+++ Driver/database/functions/DRV_Create_Rent.sql
@@ -0,0 +1,94 @@
+-- FUNCTION: public.DRV_Create_Rent(uuid, uuid)
+
+-- DROP FUNCTION IF EXISTS public."DRV_Create_Rent"(uuid, uuid);
+
+CREATE OR REPLACE FUNCTION public."DRV_Create_Rent"(
+	planid uuid,
+	userid uuid)
+    RETURNS TABLE("RentID" uuid, "Message" character varying, "Error" boolean) 
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+    ROWS 1000
+
+AS $BODY$
+DECLARE
+    "RentID" uuid;
+	vehicleid uuid := NULL;
+    "Message" character varying := '';
+    "Error" boolean := false;
+	"initdate" timestamp without time zone;
+	"finishdate" timestamp without time zone;
+	"previousdate" timestamp without time zone;
+	"pricerent" numeric(10, 2);
+BEGIN
+
+	DROP TABLE IF EXISTS temp_vehicles;
+	CREATE TEMP TABLE temp_vehicles (
+		"ID" UUID
+	);
+	
+	DROP TABLE IF EXISTS temp_plan;
+	CREATE TEMP TABLE temp_plan (
+		"ID" UUID,
+		"Days" integer,
+		"Price" numeric(10, 2)
+	);
+				
+	IF EXISTS (SELECT 1 FROM "Drivers" AS DR WHERE DR."ID" = userid AND DR."Active" = true) THEN
+		IF NOT EXISTS (SELECT 1 FROM "Rents" AS RE WHERE RE."DriverID" = userid AND RE."PricePaid" IS NULL LIMIT 1) THEN
+			IF EXISTS(SELECT 1 FROM "CnhTypes" WHERE "ID" = (SELECT "CnhID" FROM "Drivers" AS DR WHERE DR."ID" = userid AND DR."Active" = true) AND "Description" LIKE '%A%') THEN
+				INSERT INTO temp_vehicles
+				SELECT VH."ID" 
+				FROM "Vehicles" AS VH 
+				INNER JOIN "Rents" AS R ON VH."ID" = R."VehicleID" AND R."PricePaid" IS NULL;
+
+				vehicleid := (SELECT VHC."ID" FROM "Vehicles" AS VHC WHERE VHC."ID" NOT IN(SELECT TP."ID" FROM temp_vehicles AS TP) LIMIT 1);
+
+				IF(vehicleid IS NULL) THEN
+					"Message" := 'Vehicles unavailable, please try again later';
+					"Error" := true;
+				ELSE 
+					IF EXISTS (SELECT 1 FROM "Plans" WHERE "ID" = planid) THEN
+						INSERT INTO temp_plan
+						SELECT "ID", "Days", "Price" FROM "Plans" WHERE "ID" = planid;
+
+						"initdate" := (SELECT CURRENT_TIMESTAMP + INTERVAL '1 day');
+						"finishdate" := (SELECT CURRENT_DATE + INTERVAL '1 day' + (SELECT TO_CHAR((SELECT "Days" FROM temp_plan), '999') || ' days 23 hours')::INTERVAL);
+						"previousdate" := "finishdate";
+
+						"pricerent" := ((SELECT "Days" FROM temp_plan) * (SELECT "Price" FROM temp_plan));
+
+						"RentID" := uuid_generate_v4();
+
+						INSERT INTO "Rents" ("ID", "VehicleID", "PlanID", "DriverID", "InitialRent", "PreviousRent", "FinishRent", "PriceRent")
+						VALUES ("RentID", vehicleid, planid, userid, "initdate",  "previousdate", "finishdate", "pricerent");
+
+					ELSE 
+						"Message" := 'Invalid plan';
+						"Error" := true;
+					END IF;
+				END IF;
+			ELSE
+				"Message" := 'Invalid cnh';
+				"Error" := true;
+			END IF;
+		ELSE 
+			"Message" := 'finish your last rental to start another';
+			"Error" := true;
+		END IF;
+	ELSE
+		"Message" := 'Invalid driver';
+		"Error" := true;
+	END IF;
+	
+	DROP TABLE IF EXISTS temp_vehicles;
+
+	
+    RETURN QUERY
+        SELECT "RentID", "Message", "Error";
+END;
+$BODY$;
+
+ALTER FUNCTION public."DRV_Create_Rent"(uuid, uuid)
+    OWNER TO postgres;
---
+++ Driver/database/functions/DRV_Update_Rent.sql
@@ -0,0 +1,91 @@
+-- FUNCTION: public.DRV_Update_Rent(timestamp without time zone, uuid, uuid)
+
+-- DROP FUNCTION IF EXISTS public."DRV_Update_Rent"(timestamp without time zone, uuid, uuid);
+
+CREATE OR REPLACE FUNCTION public."DRV_Update_Rent"(
+	previousdate timestamp without time zone,
+	rentid uuid,
+	userid uuid)
+    RETURNS TABLE("FinishDate" timestamp without time zone, "Price" numeric, "Message" character varying, "Error" boolean) 
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+    ROWS 1000
+
+AS $BODY$
+DECLARE
+	previousrent timestamp without time zone;
+    "CalcDays" numeric;
+    "PricePlan" numeric;
+    "DayPlan" numeric;
+	"trafficTicket" numeric := 50;
+	planid uuid;
+    "Message" character varying := '';
+    "Error" boolean := false;
+BEGIN
+
+	DROP TABLE IF EXISTS temp_rent;
+	CREATE TEMP TABLE temp_rent (
+		"ID" UUID,
+		"PreviousRent" timestamp without time zone,
+		"FinishRent" timestamp without time zone,
+		"PriceRent" numeric(10, 2)
+	);
+	
+	DROP TABLE IF EXISTS temp_plan;
+	CREATE TEMP TABLE temp_plan (
+		"ID" UUID,
+		"Days" integer,
+		"Price" numeric(10, 2)
+	);
+
+	
+	IF EXISTS (SELECT 1 FROM "Drivers" AS DR WHERE DR."ID" = userid AND DR."Active" = true) THEN
+		IF EXISTS (SELECT 1 FROM "Rents" AS RE WHERE RE."ID" = rentid AND RE."PricePaid" IS NULL LIMIT 1) THEN
+		
+			UPDATE "Rents" SET "PreviousRent" = previousdate, "UpdateDate" = CURRENT_TIMESTAMP WHERE "ID" = rentid;
+			
+			INSERT INTO temp_rent
+			SELECT "ID", "PreviousRent", "FinishRent", "PriceRent" FROM "Rents" AS RE WHERE RE."ID" = rentid AND RE."PricePaid" IS NULL LIMIT 1;
+			
+			previousrent := (SELECT "PreviousRent" FROM temp_rent);
+			"FinishDate" := (SELECT "FinishRent" FROM temp_rent);
+			"Price" := (SELECT "PriceRent" FROM temp_rent);
+			
+			IF("FinishDate" > previousrent) THEN
+				"CalcDays" := (SELECT EXTRACT(day FROM ("FinishDate"::timestamp - previousrent::timestamp)));
+				planid := (SELECT "PlanID" FROM "Rents" AS RE WHERE RE."ID" = rentid AND RE."PricePaid" IS NULL LIMIT 1);
+				
+				INSERT INTO temp_plan
+				SELECT PL."ID", PL."Days", PL."Price" FROM "Plans" AS PL WHERE PL."ID" = planid;
+				
+    			"DayPlan" := (SELECT TP."Days" FROM temp_plan AS TP);
+				"PricePlan" := (SELECT TP."Price" FROM temp_plan AS TP);
+				
+				"DayPlan" := CASE WHEN "DayPlan" = 7 THEN 20 WHEN "DayPlan" = 15 THEN 40 WHEN "DayPlan" = 30 THEN 60 END; -- CHANGE PERCENTAGEM
+
+				"Price" := ROUND(("Price" + (("DayPlan" / 100.0) * ("PricePlan" * "CalcDays"))), 2);
+				"FinishDate" := previousrent;
+
+			ELSE 
+				"CalcDays" := (SELECT EXTRACT(day FROM (previousrent::timestamp - "FinishDate"::timestamp)));
+				"CalcDays" := ("CalcDays" * "trafficTicket");
+				"Price" := ("Price" + "CalcDays");
+			END IF;
+			
+		ELSE 
+			"Message" := 'Rent not found';
+			"Error" := true;
+		END IF;	
+	ELSE
+		"Message" := 'Invalid driver';
+		"Error" := true;
+	END IF;
+	
+    RETURN QUERY
+        SELECT "FinishDate", "Price",  "Message", "Error";
+END;
+$BODY$;
+
+ALTER FUNCTION public."DRV_Update_Rent"(timestamp without time zone, uuid, uuid)
+    OWNER TO postgres;
